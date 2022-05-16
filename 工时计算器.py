5823a8fa305f291e81b1d0fa28895c0fe1db833a
Peter_Ding <36361553+ares-ding@users.noreply.github.com>
2022-05-16 16:12:53 +0800

    Update 工时计算器.py

--- 工时计算器.py
+++ 工时计算器.py
@@ -27,24 +27,36 @@ while True:
     print('*************************')
     choice = int(input('*计算方式：'))
     if choice == 1:
-        # 工时计算
-        project_size = float(input('*工时计算|-项目大小：'))
-        workers_number = int(input('*工时计算|-人数：'))
-        result = Work_Hours(project_size=project_size,workers_number=workers_number)
-        print(f'*所需工时数{result}')
+        while True:
+            # 工时计算
+            project_size = float(input('*工时计算|-项目大小：'))
+            workers_number = int(input('*工时计算|-人数：'))
+            if workers_number == 0:
+                print('*[警告]人数不可为[0]，请重新输入。\n')
+                continue
+            else:
+                result = Work_Hours(project_size=project_size,workers_number=workers_number)
+                print(f'*所需工时数{result}')
+            break
         break
     elif choice == 2:
+        while True:
         # 人力计算
-        project_size = float(input('*人力计算|-项目大小：'))
-        work_hours = float(input('*工时计算|-工时数量：'))
-        result = Workers_Number(project_size=project_size,work_hours=work_hours)
-        f_result = int(result)
-        # 人数若是小数，则整数部分加1
-        if result > f_result:
-            f_result += 1
-        else:
-            f_result = result
-        print(f'*所需人工数{f_result}')
+            project_size = float(input('*人力计算|-项目大小：'))
+            work_hours = float(input('*工时计算|-工时数量：'))
+            if work_hours == 0:
+                print('*[警告]工时数不可为[0]，请重新输入。\n')
+                continue
+            else:
+                result = Workers_Number(project_size=project_size,work_hours=work_hours)
+                f_result = int(result)
+                # 人数若是小数，则整数部分加1
+                if result > f_result:
+                    f_result += 1
+                else:
+                    f_result = result
+                print(f'*所需人工数{f_result}')
+                break
         break
     else:
         print('~暂无该选项，请重新输入。')
